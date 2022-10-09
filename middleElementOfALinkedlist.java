ce6cb24f2beb5965d83d04fa5ac8d58a66ec88ed
Suraj Chaudhary <54902077+surajcool203@users.noreply.github.com>
2022-10-10 00:45:41 +0530

    Linkedlist middle element

---
+++ middleElementOfALinkedlist.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head==null)
+            return head;
+        else{
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while(fast!=null && fast.next!= null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }}
+}
